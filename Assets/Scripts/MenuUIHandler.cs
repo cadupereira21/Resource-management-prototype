690d555148ab7c50b961f72f2c419f83d8bc0d4d
Carlos Eduardo A Pereira <carloseduardo2101@gmail.com>
2024-09-26 19:03:15 -0300

    Add load scene async with loading screen when clicking on start button

--- Assets/Scripts/MenuUIHandler.cs
+++ Assets/Scripts/MenuUIHandler.cs
@@ -1,24 +1,49 @@
-using System;
 using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.Serialization;
+using UnityEngine.UI;
 
 // Sets the script to be executed later than all default scripts
 // This is helpful for UI, since other things may need to be initialized before setting the UI
 [DefaultExecutionOrder(1000)]
 public class MenuUIHandler : MonoBehaviour
 {
-    public ColorPicker ColorPicker;
+    public ColorPicker colorPicker;
+    
+    [SerializeField] private GameObject menuCanvas;
+    
+    [SerializeField] private GameObject loadingCanvas;
+    
+    [SerializeField] private Slider loadingSlider;
 
     public void NewColorSelected(Color color)
     {
         // add code here to handle when a color is selected
     }
     
-    private void Start()
-    {
-        ColorPicker.Init();
+    private void Start() {
+        colorPicker.Init();
         //this will call the NewColorSelected function when the color picker have a color button clicked.
-        ColorPicker.onColorChanged += NewColorSelected;
+        colorPicker.onColorChanged += NewColorSelected;
+        
+        menuCanvas.SetActive(true);
+        loadingCanvas.SetActive(false);
+    }
+
+    public void StartNewGame() {
+        this.StartCoroutine(LoadSceneAsync());
+    }
+
+    private IEnumerator LoadSceneAsync() {
+        menuCanvas.SetActive(false);
+        loadingCanvas.SetActive(true);
+        AsyncOperation loadAsync = SceneManager.LoadSceneAsync(1);
+        
+        if (loadAsync == null) yield break;
+        do {
+            loadingSlider.value = loadAsync.progress;
+            yield return null;
+        } while (!loadAsync.isDone);
     }
 }
